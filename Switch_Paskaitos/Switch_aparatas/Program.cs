0f2313abfdeba97bf5156e2cc918f5a0a8aebc65
AndriusJ”git config --global user.name AndriusJ” <andriaus.jurkeviciaus@gmail.com>
2022-01-16 10:27:51 +0200

    Switch

---
+++ Switch_Paskaitos/Switch_aparatas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_aparatas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pasirinkitę norimą punktą ir programą");
+            Console.WriteLine("Pasirinkimai:" +
+                "\n -    1 - Limonadas" +
+                "\n -    2 - Arbata" +
+                "\n -    3 - Kava" +
+                "\n -    4 - Nieko");
+            Console.WriteLine("Pasirinkitę:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Jūs pasirinkote limonadą");
+                    break;
+                case 2:
+                    Console.WriteLine("Jūs pasirinkote kakavą");
+                    break;
+                case 3:
+                    Console.WriteLine("Jūs pasirinkote kavą");
+                    break;
+                case 4:
+                    Console.WriteLine("Jūs nieko nepasirinkote");
+                    break;
+                default:
+                    Console.WriteLine("Klaida");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
