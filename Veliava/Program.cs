aee2e09c58bfeeeb9f980277b2e471f5916456fd
AndriusJ”git config --global user.name AndriusJ” <andriaus.jurkeviciaus@gmail.com>
2022-01-16 10:47:49 +0200

    Trispalve

---
+++ Veliava/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veliava
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įrašykite vardą: ");
+            string vardas = Console.ReadLine();
+            Console.WriteLine("Įveskitę amžių: ");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Įveskite mėgstamiausia skaičių: ");
+            double skaicius = Convert.ToDouble(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Mano vardas yra {vardas}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Mano amžius yra {amzius}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Mano skaičius yra {skaicius}");
+            Console.ReadLine();
+        }
+    }
+}
