3df20db202c6dfabde4f4dccfd7ee60a4e8736d6
AndriusJu <andriaus.jurkeviciaus@gmail.com>
2022-03-01 01:11:40 +0200

    Kraujo
    Kraujo_Bankas

---
+++ Kraujo_Bankas/Kraujo_Bankas/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VartotojuDuomenys;
+
+namespace Kraujo_Bankas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Pasirinkimas_Prisijungti_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow Pagrindinis = new MainWindow();
+            Pagrindinis.Close();
+            Prisijungimas Jungtis = new Prisijungimas();
+            Jungtis.Show();
+            Close();
+
+        }
+
+        private void Registruotis_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow Pagrindinis = new MainWindow();
+            Pagrindinis.Close();
+            Registruotis Registruotis = new Registruotis();
+            Registruotis.Show();
+            Close();
+
+        }
+        
+    }
+}
---
+++ Kraujo_Bankas/VartotojuDuomenys/Prisijungti.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VartotojuDuomenys
+{
+    public class Prisijungti
+    {
+        public static string Name { get; set; }
+        private static string Password { get; set; }
+
+        public static string PrisijungimoDuomenys = @"..\..\..\Vartotojai.txt";
+        public static void VartotojuDuomenysFailas(string prisijungimoduomenys)
+        {
+            PrisijungimoDuomenys = prisijungimoduomenys;
+
+            bool ArYraFailas = File.Exists(prisijungimoduomenys);
+            if (!ArYraFailas) { File.Create(prisijungimoduomenys); };
+
+        }
+        public Prisijungti(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+        public bool Atsakymas()
+        {
+           return File.ReadAllText(PrisijungimoDuomenys).Contains(Name + "," + Password + " ");
+
+        }
+
+
+
+
+
+    }
+}
