7a67c3e2a90a75b38336f618ce03ed8a2b2eb356
AndriusJu <andriaus.jurkeviciaus@gmail.com>
2022-03-01 12:55:01 +0200

    Kraujo_bankas
    Dar pabaiga toli

--- Kraujo_Bankas/Kraujo_Bankas/Registracija.xaml.cs
+++ Kraujo_Bankas/Kraujo_Bankas/Registracija.xaml.cs
@@ -24,77 +24,74 @@ namespace Kraujo_Bankas
         public Registruotis()
         {
             InitializeComponent();
-
+            
         }
            
 
             private void Toliau_Click(object sender, RoutedEventArgs e)
         {
 
-               // Registracija RegistracijosDuomenys = new Registracija(RegistracijosVardas.ToString(),RegistracijosSlaptazodis.ToString(),RegistracijosSlaptazodis2.ToString(),Vardas.ToString(),Pavarde.ToString(),Miestas.ToString(),Telefononumeris.ToString(),Elpastas.ToString(),GimimoData.ToString(),Lytis);
-                
-                //if (Name = true)
-                //{
-                //    MessageBox.Show("Toks vardas jau yra, bandykite kita");
-                //    RegistracijosVardas.Clear();
-                //}
+            RegistracijaDuomenys RegistracijosDuomenys = new RegistracijaDuomenys(RegistracijosVardas.Text, RegistracijosSlaptazodis.Text, RegistracijosSlaptazodis2.Text, Vardas.Text, Pavarde.Text, Miestas.Text, Telefononumeris.Text, Elpastas.Text, GimimoData.Text);
 
-                if (RegistracijosSlaptazodis.Text.Length < 6 || RegistracijosSlaptazodis != RegistracijosSlaptazodis2)
-                {
-                    MessageBox.Show("Slaptažodis pertrumpas bandykit dar kartą");
-                    RegistracijosSlaptazodis.Clear();
-                }
-
-
-                string vardas = Vardas.Text;
-                string pavarde = Pavarde.Text;
-                string miestas = Miestas.Text;
-
-
-                string telefonoNumeris = Telefononumeris.Text;
-                if (telefonoNumeris.Length == 9)
-                {
-                    MessageBox.Show("Telefono numeris pertrumpas, gal suklydote. Bandykit dar kartą");
-                    Telefononumeris.Clear();
-                }
-
-
-                string elPastas = Elpastas.Text;
-                if (!elPastas.Contains('@') && !elPastas.Contains('.'))
-                {
-                    MessageBox.Show("El. pašto adresas blogas, gal suklydote. Bandykit dar kartą");
-                    Elpastas.Clear();
-
-                }
-                string gimimoData = GimimoData.Text;
-              
-            if (gimimoData.Length < 10 )
-                {
-                    MessageBox.Show("Gimimo data pertrumpa gal suklydote. Bandykit dar kartą");
-                     GimimoData.Clear();
-
-                 }
-            else if (gimimoData.Any(char.IsDigit))
-                {
-                Console.WriteLine(gimimoData.Any(char.IsDigit));
+            if (RegistracijosDuomenys.vardas() != "001")
+            {
+                MessageBox.Show(RegistracijosDuomenys.vardas());
+                RegistracijosVardas.Clear();
             }
-                string Lytis = null;
-                if (Vyras.IsChecked == true)
-                {
-                    Lytis = "Vyras";
-                }
-                if (Moteris.IsChecked == true)
-                {
-                    Lytis = "Moteris";
-                }
 
-                File.AppendAllText(@"..\..\..\Vartotojai.txt", RegistracijosVardas.Text + "," + RegistracijosSlaptazodis.Text + " ");
+            if (RegistracijosDuomenys.slaptazodis() != "001")
+            {
+                MessageBox.Show(RegistracijosDuomenys.slaptazodis());
+                RegistracijosSlaptazodis.Clear();
+                RegistracijosSlaptazodis2.Clear();
+            }
 
-                File.AppendAllText(@"..\..\..\Donorai.txt", Vardas + "," + Pavarde + "," + Miestas + "," + Telefononumeris + "," + Elpastas + "," + GimimoData + "," + Lytis + " ");
-                Console.WriteLine("Jūs Užsiregistravote");
+            if (RegistracijosDuomenys.telefononumeris() != "001")
+            {
+                MessageBox.Show(RegistracijosDuomenys.telefononumeris());
+                Telefononumeris.Clear();
+            }
+
+            
+            if (RegistracijosDuomenys.elpastas() != "001")
+            {
+                MessageBox.Show(RegistracijosDuomenys.elpastas());
+                Elpastas.Clear();
+            }
+
+            if (RegistracijosDuomenys.gimimodata() != "001")
+            {
+                MessageBox.Show(RegistracijosDuomenys.gimimodata());
+                GimimoData.Clear();
+            }
+
+
+
+               
+             if (Vyras.IsChecked == true)
+             {
+             RegistracijosDuomenys.Lytis = "Vyras";
+             }
+             else if (Moteris.IsChecked == true)
+             {
+             RegistracijosDuomenys.Lytis = "Moteris";
+             }
+             else
+             {
+             MessageBox.Show("Pasirinkite vyras ar moteris");
+             }
+                 
+             
+
+
+
+
+             RegistracijosDuomenys.Registruotis();
+ 
+            // pataisyti kad atidarytu tik jei viskas good
              Pasirinkimas pasirinkimas = new Pasirinkimas();
              pasirinkimas.Show();
-             this.Close();
+             Close();
         }
         }
       
---
+++ Kraujo_Bankas/VartotojuDuomenys/Vartotoju_Duomenys.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VartotojuDuomenys
+{
+    public class Vartotoju_Duomenys
+    {
+
+        public static string DuomenuFailas = @"..\..\..\Vartotojai.txt";
+        public static string DonoruFailas = @"..\..\..\Donorai.txt";
+
+
+        public static void VartotojuDuomenysFailas(string duomenufailas)
+        {
+            DuomenuFailas = duomenufailas;
+
+            bool ArYraFailas = File.Exists(duomenufailas);
+            if (ArYraFailas == false) { File.Create(duomenufailas); };
+
+        }
+        public static void DonoruDuomenysFailas(string donorufailas)
+        {
+            DonoruFailas = donorufailas;
+
+            bool ArYraFailas = File.Exists(donorufailas);
+            if (ArYraFailas == false) { File.Create(donorufailas); };
+
+        }
+
+
+
+    }
+}
