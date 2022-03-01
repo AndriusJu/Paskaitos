776b4ee0c91cbc8d06b0e51d43abb8058ebf7561
AndriusJu <andriaus.jurkeviciaus@gmail.com>
2022-03-01 13:02:13 +0200

    Patikrinimui
    Nebaigta: Registracia, rezervuoti krauja, dizainas...

---
+++ Kraujo_Bankas/VartotojuDuomenys/AukotiKrauja.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VartotojuDuomenys
+{
+    internal class AukotiKrauja
+    {
+        public static string AukotasKraujas = @"..\..\..\PaaukotasKraujas.txt";
+        public static void PaaukotasKraujas(string paaukotaskraujas)
+        {
+            AukotasKraujas = paaukotaskraujas;
+
+            bool ArYraFailas = File.Exists(paaukotaskraujas);
+            if (ArYraFailas == false) { File.Create(paaukotaskraujas); };
+
+        }
+
+        public static void Aukosiu()
+        {
+            Console.WriteLine("Jūs norite paukoti kraujo");
+           //string grupe = KraujoGrupe.KuriosGrupes();
+           //File.AppendAllText(@"..\..\..\PaaukotasKraujas.txt", grupe + ",");
+           //Console.WriteLine($"Jūsų kraujo grupė yra {grupe} \n AČIŪ UŽ JŪSŲ AUKĄ !");
+           //Console.ReadLine();
+        }
+
+
+
+    }
+}
---
+++ Kraujo_Bankas/VartotojuDuomenys/Saugoti_duomenis.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VartotojuDuomenys
+{
+    public class Saugoti_duomenis
+
+    {
+        public static string KraujoGrupe { get; set; }
+
+        public static string PaaukotasKraujas = @"..\..\..\PaaukotasKraujas.txt";
+
+        public static void VartotojuDuomenysFailas(string paaukotaskraujas)
+        {
+            PaaukotasKraujas = paaukotaskraujas;
+
+            bool ArYraFailas = File.Exists(paaukotaskraujas);
+            if (!ArYraFailas) { File.Create(paaukotaskraujas); };
+
+        }
+        public static void kraujogrupe(string kraujas)
+        {
+            KraujoGrupe = kraujas;
+        }
+        public static void PaukotasKraujas()
+        {
+            VartotojuDuomenysFailas(PaaukotasKraujas);
+            File.AppendAllText(PaaukotasKraujas, Prisijungti.Name + "," + KraujoGrupe + " ");
+        }
+
+        public static string IeskomasKraujas(string kraujas)
+        {
+            VartotojuDuomenysFailas(PaaukotasKraujas);
+            int turim = 0;
+            List<string> Kraujas = new List<string>();
+            foreach (string item in File.ReadLines(PaaukotasKraujas))
+            {
+                foreach (var kit in item.Split())
+                {
+                    if (kit.Split().Contains(kraujas))
+                    {
+                        Console.WriteLine(kit[0]);
+                        turim++;
+                    }
+                }
+            }
+            foreach (var item in Kraujas)
+            {
+                Console.WriteLine(item);
+            }
+            if (turim > 0)
+            {
+                return "Šios kraujo grupės turime: " + Convert.ToString(turim);
+            }
+            else
+            {
+                return "Šios kraujo grupės neturime";
+                // Pratest kad ismestu visus kurie turi ta grupe, gal pagal miesta grupuojant
+            }
+        }
+        public static string RezervuojamasKraujas(string kraujas)
+        {
+            VartotojuDuomenysFailas(PaaukotasKraujas);
+            
+                //kol kas nezinau ka daro
+            
+            string atsakykmas = null;
+            return atsakykmas;
+        }
+    }
+}
